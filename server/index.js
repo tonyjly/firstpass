b09ee35747d89e49146800ef662523db4c2f1328
Tony Ly <tonyjly@gmail.com>
2021-05-19 23:56:32 -0700

    implement client req to server retrieve password list

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-const { addPassword, getPasswords } = require('../db/index');
+const jsonServer = require('json-server');
+// const { addPassword, getPasswords } = require('../db/index');
 
 const app = express();
+const server = jsonServer.create();
+const router = jsonServer.router('/home/tony/Nextcloud/HR-SEA16/mvp/firstpass/db/examples.json');
 
-app.get('/api', (req, res) => {
-  res.status(200).send('Yoloswag');
-});
+// app.get('/api', (req, res) => {
+//   res.status(200).send('Yoloswag');
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
+
+server.use(router)
+server.listen(PORT, () => {
+  console.log('JSON Server is running!')
 });
