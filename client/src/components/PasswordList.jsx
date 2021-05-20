22ad9690926c4dddf33dedae772c1ad6dd01f65e
Tony Ly <tonyjly@gmail.com>
2021-05-20 01:00:30 -0700

    implement password table

--- client/src/components/PasswordList.jsx
+++ client/src/components/PasswordList.jsx
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import { Card, ListGroup } from 'react-bootstrap';
+import styled from 'styled-components';
+import { Card, ListGroup, Table } from 'react-bootstrap';
+import { Eye, EyeSlash } from '@styled-icons/bootstrap';
 
 const passwords = [
-  'Application 1',
-  'Application 2',
-  'Application 3',
-  'Application 4',
-  'Application 5',
-  'Application 6',
-  'Application 7',
-  'Application 8',
+  'ToHi5UkKA^oF3EYVQcM@vFL2m&dohSSH',
+  'Qu!2!4L%k3kV&U3cRr@k5iGV2hWuL4A*',
+  '4zXUiHCkAMPMf#LWdP*HJw59dGEfs&gT',
+  '8VBpH^dzoKy#dC5w@&ZEhrT5DF4AcG$F',
+  'Ew*U%5GwA9TkYa!omN#u&x*EbfAssf7*',
+  'gXdwM4ys!J$R%&!rQ@vd3d9R8LMHBZqK',
+  'S*w*CkN4$pVTwY&BdBQbeWXP85mbVtn9',
+  'wE!QTkvKUsVA8sXF53rgPe*qRLV&x9Wx',
+  'E9SedUig*&s3v9W9J9hp!Y8@DX^Q5^br',
+  'Gtf&e8P$QXB*@aG$Nk@sXpxk#CZKgy$F',
+  '&S6&f6bNCfqqR*Fn^&9Lo63Xx45*ZHiR'
 ];
 
 const PasswordList = () => {
@@ -25,17 +30,43 @@ const PasswordList = () => {
   }, []);
 
   return (
-    <div>
-      <Card style={{ width: '18rem' }}>
-        <Card.Header>Passwords</Card.Header>
-        <ListGroup variant="flush">
+    <ListStyle>
+      <Table striped bordered hover responsive>
+        <thead className="ptable">
+          <th>#</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Password</th>
+        </thead>
+        <tbody className="ptable">
           {list.map((password) => (
-            <ListGroup.Item>{password.name}</ListGroup.Item>
+            <tr>
+              <td>{password.id}</td>
+              <td><a href={password.site} target="_blank">{password.name}</a></td>
+              <td>{password.user}</td>
+              <td>{password.password}</td>
+            </tr>
           ))}
-        </ListGroup>
-      </Card>
-    </div>
+        </tbody>
+      </Table>
+    </ListStyle>
   )
 }
 
 export default PasswordList;
+
+const ListStyle = styled.div`
+  font-weight: bold;
+  color: #5E81AC;
+`;
+
+/* CARD LISTGROUP
+  <Card style={{ width: '18rem' }}>
+    <Card.Header>Passwords</Card.Header>
+    <ListGroup variant="flush">
+      {list.map((password) => (
+        <ListGroup.Item>{password.name}</ListGroup.Item>
+      ))}
+    </ListGroup>
+  </Card>
+*/
